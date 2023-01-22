48fa3103b0e629bb2e156931fb60fb548afe64b1
rasulshirinboev <rasulboev9@gmail.com>
2023-01-22 14:46:18 -0500

    part-1

---
+++ src/main/java/com/cydeo/service/UserService.java
@@ -0,0 +1,15 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.UserDTO;
+
+
+import java.util.List;
+
+public interface UserService{
+
+    List<UserDTO> listAllUsers();
+    UserDTO findByUser(String username);
+    void save(UserDTO user);
+    void deleteByUserName(String username);
+
+}
