46ce2c69c977159ded14ff6e2e501afae86d52e3
rasulshirinboev <rasulboev9@gmail.com>
2023-01-24 19:50:18 -0500

    part-2

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -45,6 +45,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteByUserName(String username) {
 
+        userRepository.deleteByUserName(username);
+
     }
 
     @Override
