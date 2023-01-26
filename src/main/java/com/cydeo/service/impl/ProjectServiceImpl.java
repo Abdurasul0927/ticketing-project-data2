778755bcef4568c27d4ae1135300c1b77c6117d8
rasulshirinboev <rasulboev9@gmail.com>
2023-01-26 15:14:38 -0500

    part-3

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -38,6 +38,9 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     public void save(ProjectDTO dto) {
 
+        Project project = projectMapper.convertToEntity(dto);
+        projectRepository.save(project);
+
     }
 
     @Override
